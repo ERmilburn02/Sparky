13229b3988f3387bfc614436018f6b443b31e2aa
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-12-31 13:37:08 +0000

    Simple 2D Renderer

--- Sparky-Core/include/sparky.h
+++ Sparky-Core/include/sparky.h
@@ -5,6 +5,10 @@
 
 #include "graphics/buffers/buffers.h"
 
+#include "graphics/renderable2d.h"
+#include "graphics/renderer2d.h"
+#include "graphics/simple2drenderer.h"
+
 #include "maths/maths.h"
 
 #include "utils/fileUtils.h"
--- Sparky-Core/src/graphics/renderable2d.cpp
+++
@@ -1,7 +0,0 @@
-#include "graphics/renderable2d.h"
-
-namespace sparky { namespace graphics {
-
-
-
-} }
